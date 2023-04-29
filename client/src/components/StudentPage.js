1dee32468b74fe7b47ae80e180dde55ff0c4a799
Kusum Gandham <koolkusum10@gmail.com>
2023-04-29 18:25:18 -0400

    trying to get server to work

---
+++ client/src/components/StudentPage.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function StudentPage(){
+    return (
+        <hi>Hello Student</hi>
+    );
+}
+export default StudentPage;
