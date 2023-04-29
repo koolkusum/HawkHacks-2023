a567f67845f78134694a24b93568fb1b69d0e96e
Kusum Gandham <koolkusum10@gmail.com>
2023-04-29 16:41:23 -0400

    more to frontend

---
+++ client/src/components/Login.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {getUser} from '../api';
+import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const nav = useNavigate();
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try {
+        const response = await getUser(email,password);//if response status code
+        sessionStorage.setItem('email', email);
+        sessionStorage.setItem('password', password);
+
+        if (response.status===200)
+        {
+          nav('/TaskPlanner');
+        }
+        //console.log(response); // do something with the user data, like redirect to a new page
+      } catch (error) {
+        console.error(error); // handle errors
+      }
+    };
+  
+    return (
+      <div className="login">
+         <h1>Login</h1> 
+        <form onSubmit={handleLogin}>
+          <label>
+            {/* Email: */}
+            <input
+              type="email"
+              value={email}
+              placeholder='Email Address'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            {/* Password: */}
+            <input
+              type="password"
+              value={password}
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">Login</button>
+          <p>Don't have an account? <Link to="/Signup">Sign up</Link></p>
+        </form>
+  
+      </div>
+    );
+  }
+  
+  export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -11,18 +11,84 @@ async function Signup(){
     const nav = useNavigate();
 
     const handleSubmit =async(event) =>{
-        event.preventDefault();
-        console.log ('Passwords do not match');
-        return;
-    }
+        if (password !== confirmPassword) {
+            console.log('Passwords do not match');
+            return;
+          }
+    
     try{
         const user = {name, email, password};
         sessionStorage.setItem ('email',email);
         sessionStorage.setItem('password',password);
         const response = await createUser(user);
-
+        if (response.status===200)
+        {
+            nav('/StudentPage')
+        }
     }
     catch(error){
 
     }
-};
+    }
+
+return(
+    <div className = "Signup">
+        <h1>Sign up</h1>
+        <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input
+          className="form-control"
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          className="form-control"
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          className="form-control"
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          className="form-control"
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Sign Up</button>
+      <div className="mt-3">
+        Already have an account? <Link to="/Login">Log in</Link>
+      </div>
+    </form>
+    </div>
+);
+}
+export default Signup;
