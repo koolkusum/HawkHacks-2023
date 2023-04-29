822d90b4de5875f6f462f2e44336ee98660bd76c
Kusum Gandham <koolkusum10@gmail.com>
2023-04-29 19:54:29 -0400

    ui updates

--- client/src/api.js
+++ client/src/api.js
@@ -9,3 +9,7 @@ export const getUsers = async(email,password)=>{
     const response = await axios.get(`${API_URL}/users/getUsers/${email}/${password}`);
     return response;
 }
+export const addCourse = async(email, password, goal) => {
+    const response = await axios.put(`${API_URL}/users/updateGoal/${email}/${password}/${goal}`);
+    return response;
+}
